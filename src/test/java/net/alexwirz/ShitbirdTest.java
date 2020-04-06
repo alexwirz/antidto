e6ec197aac2d4b3ca8348b949f5573a6b1095228
Alexander Wirz <alex.wirz@gmail.com>
2020-04-06 19:00:54 +0200

    Add first test

---
+++ src/test/java/net/alexwirz/ShitbirdTest.java
@@ -0,0 +1,12 @@
+package net.alexwirz;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ShitbirdTest {
+    @Test
+    void testsWork() {
+        assertThat(true).isTrue();
+    }
+}
